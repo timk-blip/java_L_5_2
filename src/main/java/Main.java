a0fe38c8862cf3a6891fb755e91370b3598e6205
kiselev <kiselev@NTC-36>
2020-06-29 16:45:41 +0300

    fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,8 @@ public class Main {
         long amount = 1000_60;
         boolean registered = true;
 
-        service.calculate(amount, registered)
-        /*long res = service.calculate(amount, registered);
-        System.out.println(res);*/
+        long res = service.calculate(amount, registered);
+        System.out.println(res);
 
     }
 }
